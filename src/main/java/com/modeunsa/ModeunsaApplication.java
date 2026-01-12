2b847b3c1e8df7395b75a8c2f268854181283bf4
sohee52 <wneh2023@naver.com>
2026-01-12 16:59:58 +0900

    chore: config before merge

--- src/main/java/com/modeunsa/ModeunsaApplication.java
+++ src/main/java/com/modeunsa/ModeunsaApplication.java
@@ -3,10 +3,6 @@ package com.modeunsa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author : JAKE
- * @date : 26. 1. 9.
- */
 @SpringBootApplication
 public class ModeunsaApplication {
 
