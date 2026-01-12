c863359fd2f8c3ba922997b03da6bba13be31449
rkdls8268 <rkdls8268@gmail.com>
2026-01-12 14:52:52 +0900

    [#5] feat: missingJavadocType 주석처리

--- src/main/java/com/modeunsa/ModeunsaApplication.java
+++ src/main/java/com/modeunsa/ModeunsaApplication.java
@@ -3,15 +3,10 @@ package com.modeunsa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author : JAKE
- * @date : 26. 1. 9.
- */
 @SpringBootApplication
 public class ModeunsaApplication {
 
   public static void main(String[] args) {
-    // test 수정이 될까요?
     SpringApplication.run(ModeunsaApplication.class, args);
   }
 }
