a842a5e66328d81dfc13685150d1e2a106c7559b
OneE <bestoneeee@naver.com>
2026-01-12 20:40:27 +0900

    [#74] refactor: 주문 상태 enum에 설명 필드 추가

--- src/main/java/com/modeunsa/boundedcontext/order/domain/OrderStatus.java
+++ src/main/java/com/modeunsa/boundedcontext/order/domain/OrderStatus.java
@@ -1,18 +1,26 @@
 package com.modeunsa.boundedcontext.order.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
 public enum OrderStatus {
-    PENDING_PAYMENT,        // 결제 대기 (주문서 생성)
-    PAYMENT_FAILED,         // 결제 실패
-    PAID,                   // 결제 완료 (상품 준비 중)
-    SHIPPING,               // 배송 중
-    DELIVERED,              // 배송 완료
-    PURCHASE_CONFIRMED,     // 구매 확정 (정산 가능)
+    PENDING_PAYMENT("결제 대기"),        // 주문서 생성
+    PAYMENT_FAILED("결제 실패"),
+    PAID("결제 완료"),                   // 상품 준비 중
+    SHIPPING("배송 중"),
+    DELIVERED("배송 완료"),
+    PURCHASE_CONFIRMED("구매 확정"),     // 정산 가능
 
     // 주문 취소 (배송 전)
-    CANCELLED_BY_USER,      // 사용자 취소
-    CANCELLED_BY_SELLER,    // 판매자 취소
+    CANCELLED_BY_USER("사용자 취소"),
+    CANCELLED_BY_SELLER("판매자 취소"),
 
     // 환불 (배송 후)
-    REFUND_REQUESTED,       // 환불 요청됨
-    REFUNDED,               // 환불 완료
+    REFUND_REQUESTED("환불 요청됨"),
+    REFUNDED("환불 완료");
+
+    private final String description;
 }
