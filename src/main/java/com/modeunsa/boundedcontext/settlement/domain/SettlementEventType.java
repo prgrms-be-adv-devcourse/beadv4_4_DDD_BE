c062cf4311a416fabcd30cf9472b8b2869c88e5b
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2026-01-12 14:31:47 +0900

    [#65] feat: 정산 타입 enum을 영문으로 처리하도록 수정

--- src/main/java/com/modeunsa/boundedcontext/settlement/domain/SettlementEventType.java
+++ src/main/java/com/modeunsa/boundedcontext/settlement/domain/SettlementEventType.java
@@ -1,6 +1,17 @@
 package com.modeunsa.boundedcontext.settlement.domain;
 
 public enum SettlementEventType {
-    정산_상품판매_수수료,
-    정산_상품판매_대금
+    SETTLEMENT_PRODUCT_SALES_FEE("정산_상품판매_수수료"),
+    SETTLEMENT_PRODUCT_SALES_AMOUNT("정산_상품판매_대금");
+
+    private final String description;
+
+    SettlementEventType(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
 }
