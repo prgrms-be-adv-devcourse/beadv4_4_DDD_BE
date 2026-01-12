82993aa3aacf283bc61f0998c1487591a3d20579
OneE <bestoneeee@naver.com>
2026-01-12 16:36:42 +0900

    [#74] feat: 주문멤버 엔티티 추가

---
+++ src/main/java/com/modeunsa/boundedcontext/order/domain/OrderMember.java
@@ -0,0 +1,31 @@
+package com.modeunsa.boundedcontext.order.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "ORDER_MEMBER")
+public class OrderMember {
+    @Id @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(name = "member_name", nullable = false, length = 20)
+    private String memberName;
+
+    @Column(name = "member_phone", nullable = false, length = 20)
+    private String memberPhone;
+
+    @Column(name = "zipcode", length = 10)
+    private String zipcode;
+
+    @Column(name = "address_detail", length = 200)
+    private String addressDetail;
+}
