d9e10832d9913e0ce85caf2c0872107fb1ea0835
kjuiop <copying_y@naver.com>
2026-01-12 13:19:16 +0900

    [#63] feat: 결제 회원 도메인 생성

---
+++ src/main/java/com/modeunsa/boundedcontext/payment/domain/PaymentMember.java
@@ -0,0 +1,47 @@
+package com.modeunsa.boundedcontext.payment.domain;
+
+import com.modeunsa.boundedcontext.payment.domain.type.MemberStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author : JAKE
+ * @date : 26. 1. 12.
+ */
+@Entity
+@Table(name = "PAYMENT_MEMBER")
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class PaymentMember {
+
+  @Id
+  private long id;
+
+  @Column(nullable = false, unique = true)
+  private String email;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false, unique = true)
+  private String customerKey;
+
+  @Builder.Default
+  @Column(nullable = false, length = 20)
+  @Enumerated(EnumType.STRING)
+  private MemberStatus status = MemberStatus.활성;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/modeunsa/boundedcontext/payment/domain/type/MemberStatus.java
@@ -0,0 +1,17 @@
+package com.modeunsa.boundedcontext.payment.domain.type;
+
+import lombok.Getter;
+
+/**
+ * @author : JAKE
+ * @date : 26. 1. 12.
+ */
+@Getter
+public enum MemberStatus {
+
+  활성,
+
+  정지,
+
+  탈퇴;
+}
