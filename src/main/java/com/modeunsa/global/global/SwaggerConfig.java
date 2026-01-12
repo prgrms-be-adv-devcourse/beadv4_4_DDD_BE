5f0633411b2a03f6e51374014d65456c67fb143b
OneE <bestoneeee@naver.com>
2026-01-12 15:27:12 +0900

    [#70] feat: Swagger API 문서 기본 설정과 Bearer JWT 인증 추가

---
+++ src/main/java/com/modeunsa/global/global/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.modeunsa.global.global;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        Server devServer = new Server();
+        devServer.setUrl("/");
+
+        Info info = new Info()
+                .title("Modeunsa API")
+                .version("v1.0.0")
+                .description("프로그래머스 단기심화4 DDD 세미프로젝트 스웨거 API 문서입니다.");
+
+        // SecurityScheme 설정
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // SecurityRequirement 설정
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(devServer)
+                .addSecurityItem(securityRequirement)
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", bearerAuth)
+                );
+    }
+}
