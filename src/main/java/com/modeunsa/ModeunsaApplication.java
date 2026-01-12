a207fd3a631eb62afa9d488efe008c7dcc8ff08b
rkdls8268 <rkdls8268@gmail.com>
2026-01-12 16:26:29 +0900

    [#66] feat: BaseEntity 추가

--- src/main/java/com/modeunsa/ModeunsaApplication.java
+++ src/main/java/com/modeunsa/ModeunsaApplication.java
@@ -2,11 +2,13 @@ package com.modeunsa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * @author : JAKE
  * @date : 26. 1. 9.
  */
+@EnableJpaAuditing
 @SpringBootApplication
 public class ModeunsaApplication {
 
