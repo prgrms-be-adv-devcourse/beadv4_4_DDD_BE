3d39fe36a59d8c8c66678759b90d1c73974cf0f8
OneE <bestoneeee@naver.com>
2026-01-12 12:27:51 +0900

    [#78] feat: 전역 예외 처리와 공통 응답 포맷 구현

---
+++ src/main/java/com/modeunsa/global/exception/ExceptionAdvice.java
@@ -0,0 +1,60 @@
+package com.modeunsa.global.exception;
+
+import com.modeunsa.global.response.ApiResponse;
+import com.modeunsa.global.status.ErrorStatus;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.ConversionFailedException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler
+    public ResponseEntity<ApiResponse> validation(ConstraintViolationException e) {
+        String errorMessage = e.getConstraintViolations().stream()
+                .map(constraintViolation -> constraintViolation.getMessage())
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("ConstraintViolationException 추출 도중 에러 발생"));
+
+        return ApiResponse.onFailure(ErrorStatus.VALIDATION_ERROR, errorMessage);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        e.printStackTrace();
+
+        String errorMessage = e.getBindingResult().getFieldError().getDefaultMessage();
+        return ApiResponse.onFailure(ErrorStatus.VALIDATION_ERROR, errorMessage);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ApiResponse> handleNoResourceFoundException(NoResourceFoundException e) {
+        e.printStackTrace();
+
+        return ApiResponse.onFailure(ErrorStatus._NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleException(Exception e) {
+        log.error("Unhandled Exception: ", e);
+        return ApiResponse.onFailure((ErrorStatus._INTERNAL_SERVER_ERROR));
+    }
+
+    @ExceptionHandler(GeneralException.class)
+    public ResponseEntity<ApiResponse> handleGeneralException(GeneralException e) {
+        e.printStackTrace();
+
+        return ApiResponse.onFailure(e.getErrorStatus(), e.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, ConversionFailedException.class})
+    public ResponseEntity<ApiResponse> handleConversionFailedException(Exception e) {
+        return ApiResponse.onFailure((ErrorStatus._BAD_REQUEST));
+    }
+}
---
+++ src/main/java/com/modeunsa/global/exception/GeneralException.java
@@ -0,0 +1,28 @@
+package com.modeunsa.global.exception;
+
+import com.modeunsa.global.status.ErrorStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GeneralException extends RuntimeException {
+
+    private ErrorStatus errorStatus;
+
+    public GeneralException(String message) {
+        super(message);
+        this.errorStatus = ErrorStatus._INTERNAL_SERVER_ERROR;
+    }
+
+    public GeneralException(String message, Throwable cause) {
+        super(message, cause);
+        this.errorStatus = ErrorStatus._INTERNAL_SERVER_ERROR;
+    }
+
+    public GeneralException(Throwable cause) {
+        super(cause.getMessage(), cause);
+        this.errorStatus = ErrorStatus._INTERNAL_SERVER_ERROR;
+    }
+
+}
