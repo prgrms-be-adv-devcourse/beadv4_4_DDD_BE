c40709c827e33238b915b03647b4644493b01cc0
sohee52 <wneh2023@naver.com>
2026-01-14 19:25:39 +0900

    [#12] fix: 공개 API 범위 축소 및 개발 환경 보안 설정 분리

--- src/main/java/com/modeunsa/global/config/SecurityConfig.java
+++ src/main/java/com/modeunsa/global/config/SecurityConfig.java
@@ -2,17 +2,19 @@ package com.modeunsa.global.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!dev")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
   private static final String[] PERMIT_URLS = {
-      "/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/v1/**"
+      "/h2-console/**", "/swagger-ui/**", "/v3/api-docs/**", "/api/v1/auths/oauth/**"
   };
 
   @Bean
