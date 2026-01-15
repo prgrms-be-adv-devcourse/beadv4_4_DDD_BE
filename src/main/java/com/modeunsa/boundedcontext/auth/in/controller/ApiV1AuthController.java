2305604546a272e30cb17341cfe90cee9e46c6a9
sohee52 <wneh2023@naver.com>
2026-01-15 20:45:20 +0900

    [#12] style: 컨벤션에 맞게 주석 스타일 변경

--- src/main/java/com/modeunsa/boundedcontext/auth/in/controller/ApiV1AuthController.java
+++ src/main/java/com/modeunsa/boundedcontext/auth/in/controller/ApiV1AuthController.java
@@ -50,9 +50,11 @@ public class ApiV1AuthController {
   ) {
     OAuthProvider oauthProvider = findProvider(provider);
 
-    // TODO: 실제 구현 시에는 provider와 code를 Facade에 넘겨서
-    // 1. 소셜 회원 정보 조회 -> 2. 회원가입/로그인 처리 -> 3. MemberId 추출 과정을 거쳐야 합니다.
-    // 현재는 구조 설명을 위해 임의의 ID(1L)와 Role(USER)을 넘깁니다.
+    /**
+    /* TODO: 실제 구현 시에는 provider와 code를 Facade에 넘겨서
+    /* 1. 소셜 회원 정보 조회 -> 2. 회원가입/로그인 처리 -> 3. MemberId 추출 과정을 거쳐야 합니다.
+    /* 현재는 구조 설명을 위해 임의의 ID(1L)와 Role(USER)을 넘깁니다.
+     */
     TokenResponse tokenResponse = authFacade.login(1L, MemberRole.MEMBER);
 
     return ApiResponse.onSuccess(SuccessStatus.AUTH_LOGIN_SUCCESS, tokenResponse);
