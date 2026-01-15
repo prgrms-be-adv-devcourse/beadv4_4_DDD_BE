f8dd39d7c43773a4c127e388d0223f5b1d00ebcc
sohee52 <wneh2023@naver.com>
2026-01-15 10:42:43 +0900

    [#12] fix: Spring Boot 4 환경에서 permit-urls 설정 바인딩 방식 수정

--- src/main/java/com/modeunsa/global/config/SecurityConfig.java
+++ src/main/java/com/modeunsa/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.modeunsa.global.config;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
