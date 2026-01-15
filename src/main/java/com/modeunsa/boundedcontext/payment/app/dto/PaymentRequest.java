0129bba3b5ec117bff0d5bf5cf0bb7c14c931ea3
kjuiop <copying_y@naver.com>
2026-01-15 23:24:34 +0900

    [#136] test: 결제 요청 API Test 코드 작성

--- src/main/java/com/modeunsa/boundedcontext/payment/app/dto/PaymentRequest.java
+++ src/main/java/com/modeunsa/boundedcontext/payment/app/dto/PaymentRequest.java
@@ -4,12 +4,14 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @ToString
 public class PaymentRequest {
   @NotNull private Long orderId;
