fe0af3d360db4f329f66039a82dfef7460828899
rkdls8268 <rkdls8268@gmail.com>
2026-01-15 17:58:52 +0900

    [#36] feat: event record로 수정

--- src/main/java/com/modeunsa/shared/product/event/ProductCreatedEvent.java
+++ src/main/java/com/modeunsa/shared/product/event/ProductCreatedEvent.java
@@ -1,12 +1,5 @@
 package com.modeunsa.shared.product.event;
 
 import com.modeunsa.shared.product.dto.ProductResponse;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class ProductCreatedEvent {
-
-  private final ProductResponse productResponse;
-}
+public record ProductCreatedEvent(ProductResponse productResponse) {}
--- src/main/java/com/modeunsa/shared/product/event/ProductUpdatedEvent.java
+++ src/main/java/com/modeunsa/shared/product/event/ProductUpdatedEvent.java
@@ -1,12 +1,5 @@
 package com.modeunsa.shared.product.event;
 
 import com.modeunsa.shared.product.dto.ProductResponse;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class ProductUpdatedEvent {
-
-  private final ProductResponse productResponse;
-}
+public record ProductUpdatedEvent(ProductResponse productResponse) {}
