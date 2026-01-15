4b4d9ea17e61088c7e89952f1e3427eefc08c0a6
JeongIn <41246605+kjuiop@users.noreply.github.com>
2026-01-15 18:17:17 +0900

    [FEAT] 주문 취소, 결제 실패 시 환불로 인해 예치금이 증가된다. (#7)
    * [#7] feat: 결제 실패에 대한 환불처리
    
    * [#7] feat: 주문 취소에 대한 환불 기능
    
    * [#7] test: 결제취소, 주문취소에 대한 환불 테스트케이스 작성
    
    * [#7] feat: 환불 시 holder 의 잔액이 없는 경우는 에러처리를 한다.
    
    * [#7] fix: enum 에 대한 생성자 규칙 변경
    
    * [#7] test: holder 잔액 부족에 의한 에러 테스트 케이스 추가

---
+++ src/main/java/com/modeunsa/boundedcontext/payment/app/usecase/PaymentRefundUseCase.java
@@ -0,0 +1,38 @@
+package com.modeunsa.boundedcontext.payment.app.usecase;
+
+import com.modeunsa.boundedcontext.payment.app.support.PaymentAccountSupport;
+import com.modeunsa.boundedcontext.payment.domain.entity.PaymentAccount;
+import com.modeunsa.boundedcontext.payment.domain.types.PaymentEventType;
+import com.modeunsa.boundedcontext.payment.domain.types.ReferenceType;
+import com.modeunsa.boundedcontext.payment.domain.types.RefundEventType;
+import com.modeunsa.global.exception.GeneralException;
+import com.modeunsa.global.status.ErrorStatus;
+import com.modeunsa.shared.payment.dto.PaymentDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentRefundUseCase {
+
+  private final PaymentAccountSupport paymentAccountSupport;
+
+  public void execute(PaymentDto payment, RefundEventType refundEventType) {
+
+    PaymentEventType eventType = PaymentEventType.fromRefundEventType(refundEventType);
+
+    PaymentAccount holderAccount = paymentAccountSupport.getHolderAccount();
+
+    if (!holderAccount.canPayOrder(payment.getPgPaymentAmount())) {
+      throw new GeneralException(ErrorStatus.PAYMENT_INSUFFICIENT_BALANCE);
+    }
+
+    PaymentAccount buyerAccount =
+        paymentAccountSupport.getPaymentAccountByMemberId(payment.getBuyerId());
+
+    holderAccount.debit(
+        payment.getPgPaymentAmount(), eventType, payment.getOrderId(), ReferenceType.PAYMENT);
+    buyerAccount.credit(
+        payment.getPgPaymentAmount(), eventType, payment.getOrderId(), ReferenceType.PAYMENT);
+  }
+}
