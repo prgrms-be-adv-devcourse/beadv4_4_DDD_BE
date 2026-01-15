426981bdcd74406031b5508c6dabdba295cddfb8
Sohee Kim <wneh2023@naver.com>
2026-01-15 14:14:20 +0900

    [FEAT] #128 초기 회원 데이터 생성 (#132)
    * [#12] feat: 회원 및 판매자 초기 데이터 추가
    
    * [#12] chore: 사라진 Order 관련 ErrorStatus 추가

---
+++ src/main/java/com/modeunsa/boundedcontext/member/out/repository/MemberSellerRepository.java
@@ -0,0 +1,7 @@
+package com.modeunsa.boundedcontext.member.out.repository;
+
+import com.modeunsa.boundedcontext.member.domain.entity.MemberSeller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberSellerRepository extends JpaRepository<MemberSeller, Long> {
+}
