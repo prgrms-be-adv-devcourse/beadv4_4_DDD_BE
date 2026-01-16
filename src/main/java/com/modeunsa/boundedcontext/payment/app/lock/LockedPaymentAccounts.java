f03c0039cebc2ea9b69a33c882c0289c55e7f259
kjuiop <copying_y@naver.com>
2026-01-16 16:46:44 +0900

    [#117] style: LockManager 에 대한 설명을 주석처리하여 기재

--- src/main/java/com/modeunsa/boundedcontext/payment/app/lock/LockedPaymentAccounts.java
+++ src/main/java/com/modeunsa/boundedcontext/payment/app/lock/LockedPaymentAccounts.java
@@ -8,7 +8,13 @@ import com.modeunsa.global.lock.LockedEntities;
 import java.util.Map;
 
 /*
- * 데드락 방지를 위해 항상 작은 ID 부터 락을 획득 하도록 합니다.
+ * 동시 2개의 트랜잭션이 1->100, 100->1 순서로 각각 락을 획득하려할 때 데드락이 발생할 수 있습니다.
+ * 1번 트랜잭션이 1번 락을 걸고 100번 락을 기다리는 상황에서
+ * 2번 트랜잭션이 100번 락을 걸고 1번 락을 기다리게 되면 서로가 상대방이 락을 풀어주기만을 기다리게 되어 무한 대기 상태에 빠지게 됩니다.
+ * 이에 애플리케이션 내부에서 락을 획득하는 순서를 강제하여 데드락 발생 가능성을 제거합니다.
+ *
+ * 해당 클래스에서는 락을 획득한 PaymentAccount 엔티티들을 보관합니다.
+ * 락은 로컬변수에서 사용됨으로, 트랜잭션이 종료되면 자동으로 해제됩니다.
  */
 public record LockedPaymentAccounts(Map<Long, PaymentAccount> accounts)
     implements LockedEntities<PaymentAccount, Long> {
--- src/main/java/com/modeunsa/boundedcontext/payment/app/lock/PaymentAccountLockManager.java
+++ src/main/java/com/modeunsa/boundedcontext/payment/app/lock/PaymentAccountLockManager.java
@@ -10,6 +10,16 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+/*
+ * 동시 2개의 트랜잭션이 1->100, 100->1 순서로 각각 락을 획득하려할 때 데드락이 발생할 수 있습니다.
+ * 1번 트랜잭션이 1번 락을 걸고 100번 락을 기다리는 상황에서
+ * 2번 트랜잭션이 100번 락을 걸고 1번 락을 기다리게 되면 서로가 상대방이 락을 풀어주기만을 기다리게 되어 무한 대기 상태에 빠지게 됩니다.
+ * 이에 애플리케이션 내부에서 락을 획득하는 순서를 강제하여 데드락 발생 가능성을 제거합니다.
+ *
+ * 해당 클래스에서는 PaymentAccount 엔티티들에 대해 락을 획득하는 기능을 제공합니다.
+ * LinkedHashMap를 이용하여 락 획득 순서를 보장합니다.
+ * ID 순서대로 작은 ID부터 락을 획득합니다.
+ */
 @Component
 @RequiredArgsConstructor
 public class PaymentAccountLockManager implements EntityLockManager<PaymentAccount, Long> {
