539fadf5eb1f3b746fd72048e34caba60e8c96be
sohee52 <wneh2023@naver.com>
2026-01-16 17:57:35 +0900

    [#145] refactor: 계좌번호 정규식을 상수로 추출

--- src/main/java/com/modeunsa/shared/member/dto/SellerRegisterRequest.java
+++ src/main/java/com/modeunsa/shared/member/dto/SellerRegisterRequest.java
@@ -7,6 +7,8 @@ public record SellerRegisterRequest(
     @NotBlank String businessName,
     @NotBlank String representativeName,
     @NotBlank String settlementBankName,
-    @NotBlank @Pattern(regexp = "^[0-9-]{10,20}$", message = "계좌번호 형식이 올바르지 않습니다")
-        String settlementBankAccount,
-    @NotBlank String businessLicenseUrl) {}
+    @NotBlank @Pattern(regexp = BANK_ACCOUNT_REGEX, message = "계좌번호 형식이 올바르지 않습니다")
+    String settlementBankAccount,
+    @NotBlank String businessLicenseUrl) {
+        public static final String BANK_ACCOUNT_REGEX = "^[0-9-]{10,20}$";
+}
