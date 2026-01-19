40c8df5e1f4cf1b181f9db71309f5c193dd96d5a
rkdls8268 <rkdls8268@gmail.com>
2026-01-19 14:37:10 +0900

    [#43] refactor: response class -> record 로 변경

--- src/main/java/com/modeunsa/shared/product/dto/ProductDetailResponse.java
+++ src/main/java/com/modeunsa/shared/product/dto/ProductDetailResponse.java
@@ -6,29 +6,22 @@ import com.modeunsa.boundedcontext.product.domain.ProductStatus;
 import com.modeunsa.boundedcontext.product.domain.SaleStatus;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-public class ProductDetailResponse {
-  private Long id;
-  private Long sellerId;
-  private String name;
-  private ProductCategory category;
-  private String description;
-  private BigDecimal price;
-  private BigDecimal salePrice;
-  private ProductCurrency currency;
-  private ProductStatus productStatus;
-  private SaleStatus saleStatus;
-  private int quantity;
-  private Boolean isFavorite;
-  private int favoriteCount;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
-  private Long createdBy;
-  private Long updatedBy;
-}
+public record ProductDetailResponse(
+    Long id,
+    Long sellerId,
+    String name,
+    ProductCategory category,
+    String description,
+    BigDecimal price,
+    BigDecimal salePrice,
+    ProductCurrency currency,
+    ProductStatus productStatus,
+    SaleStatus saleStatus,
+    int quantity,
+    Boolean isFavorite,
+    int favoriteCount,
+    LocalDateTime createdAt,
+    LocalDateTime updatedAt,
+    Long createdBy,
+    Long updatedBy) {}
