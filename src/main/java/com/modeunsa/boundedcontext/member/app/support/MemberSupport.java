1ee104bf750902637e50b500a5c02e353be5f6fa
sohee52 <wneh2023@naver.com>
2026-01-20 20:37:24 +0900

    [#19] chore: 불필요한 코드 제거

--- src/main/java/com/modeunsa/boundedcontext/member/app/support/MemberSupport.java
+++ src/main/java/com/modeunsa/boundedcontext/member/app/support/MemberSupport.java
@@ -2,7 +2,6 @@ package com.modeunsa.boundedcontext.member.app.support;
 
 import com.modeunsa.boundedcontext.member.domain.entity.Member;
 import com.modeunsa.boundedcontext.member.domain.entity.MemberProfile;
-import com.modeunsa.boundedcontext.member.out.repository.MemberProfileRepository;
 import com.modeunsa.boundedcontext.member.out.repository.MemberRepository;
 import com.modeunsa.global.exception.GeneralException;
 import com.modeunsa.global.status.ErrorStatus;
@@ -15,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class MemberSupport {
   private final MemberRepository memberRepository;
-  private final MemberProfileRepository memberProfileRepository;
 
   public Member getMember(Long memberId) {
     return memberRepository
