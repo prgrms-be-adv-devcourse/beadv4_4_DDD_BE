62198b4ba59a55480abfdc154f5defe5be640acf
sohee52 <wneh2023@naver.com>
2026-01-20 09:57:47 +0900

    [#182] test: S3 Config 테스트 코드 추가

---
+++ src/test/java/com/modeunsa/global/config/S3ConfigTest.java
@@ -0,0 +1,27 @@
+package com.modeunsa.global.config;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@SpringBootTest
+class S3ConfigTest {
+
+  @Autowired
+  private S3Client s3Client;
+
+  @Autowired
+  private S3Properties s3Properties;
+
+  @Test
+  void S3ConnectTest() {
+    assertDoesNotThrow(() -> {
+      s3Client.listObjects(builder ->
+          builder.bucket(s3Properties.s3().bucket()).maxKeys(1)
+      );
+    });
+  }
+}
