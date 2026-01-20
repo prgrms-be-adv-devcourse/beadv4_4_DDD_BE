c61d659bfabe4d70d38fd5a46221b900941631c2
OneE <bestoneeee@naver.com>
2026-01-20 12:50:48 +0900

    [#184] feat: OrdetItemDto에 id추가

--- src/main/java/com/modeunsa/shared/order/dto/OrderItemDto.java
+++ src/main/java/com/modeunsa/shared/order/dto/OrderItemDto.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class OrderItemDto {
+  private Long id; // 구매상품 ID
   private Long productId; // 상품 ID
   private Long sellerId; // 판매자 ID
   private int quantity; // 구매 수량
