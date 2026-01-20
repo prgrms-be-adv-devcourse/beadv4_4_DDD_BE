646861c58f6c352abf19293b5cc190b284fa39ad
sohee52 <wneh2023@naver.com>
2026-01-20 21:30:11 +0900

    [#19] feat: 회원 기본 정보 수정에 도메인 이벤트 발행 추가, 변수명 통일성 있게 변경

--- src/main/java/com/modeunsa/boundedcontext/member/app/usecase/MemberBasicInfoUpdateUseCase.java
+++ src/main/java/com/modeunsa/boundedcontext/member/app/usecase/MemberBasicInfoUpdateUseCase.java
@@ -3,20 +3,27 @@ package com.modeunsa.boundedcontext.member.app.usecase;
 import com.modeunsa.boundedcontext.member.app.support.MemberSupport;
 import com.modeunsa.boundedcontext.member.domain.entity.Member;
 import com.modeunsa.shared.member.dto.request.MemberBasicInfoUpdateRequest;
+import com.modeunsa.shared.member.event.MemberBasicInfoUpdatedEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MemberBasicInfoUpdateUseCase {
-  private final MemberSupport memberReader;
+  private final MemberSupport memberSupport;
+  private final ApplicationEventPublisher eventPublisher;
 
   public void execute(Long memberId, MemberBasicInfoUpdateRequest request) {
-    Member member = memberReader.getMember(memberId);
+    Member member = memberSupport.getMember(memberId);
 
     member
         .updateRealName(request.getRealName())
         .updatePhoneNumber(request.getPhoneNumber())
         .updateEmail(request.getEmail());
+
+    eventPublisher.publishEvent(
+        MemberBasicInfoUpdatedEvent.of(
+            memberId, request.getRealName(), request.getPhoneNumber(), request.getEmail()));
   }
 }
