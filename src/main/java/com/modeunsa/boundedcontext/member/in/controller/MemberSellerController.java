4027d0d3074b1c43d7e026a0c91d65c78751f786
sohee52 <wneh2023@naver.com>
2026-01-21 07:42:08 +0900

    [#19] fix: User user가 아닌 Long memberId로 변경

--- src/main/java/com/modeunsa/boundedcontext/member/in/controller/MemberSellerController.java
+++ src/main/java/com/modeunsa/boundedcontext/member/in/controller/MemberSellerController.java
@@ -1,22 +1,21 @@
 package com.modeunsa.boundedcontext.member.in.controller;
 
 import com.modeunsa.boundedcontext.member.app.facade.MemberFacade;
-import com.modeunsa.global.exception.GeneralException;
 import com.modeunsa.global.response.ApiResponse;
-import com.modeunsa.global.status.ErrorStatus;
 import com.modeunsa.global.status.SuccessStatus;
 import com.modeunsa.shared.member.dto.SellerRegisterRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Seller", description = "판매자 관련 API")
 @RestController
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
@@ -25,21 +24,14 @@ public class MemberSellerController {
   private final MemberFacade memberFacade;
 
   /** 판매자 등록 요청 */
-  @PostMapping(value = "/sellers/register", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  @Operation(summary = "판매자 등록 요청", description = "일반 회원이 판매자로 전환하기 위해 등록을 요청합니다.")
+  @PostMapping(value = "/sellers/register")
   public ResponseEntity<ApiResponse> registerSeller(
-      @AuthenticationPrincipal User user,
-      @RequestPart("request") @Valid SellerRegisterRequest request,
+      @AuthenticationPrincipal Long memberId,
+      @RequestBody @Valid SellerRegisterRequest request
       // TODO: MultipartFile 변경 예정
-      @RequestPart(value = "licenseImage", required = true) String licenseImage) {
-    String username = user.getUsername();
-    Long memberId;
-    try {
-      memberId = Long.parseLong(username);
-    } catch (NumberFormatException e) {
-      throw new GeneralException(ErrorStatus.MEMBER_INVALID_ID_FORMAT);
-    }
-
-    memberFacade.registerSeller(memberId, request, licenseImage);
+      ) {
+    memberFacade.registerSeller(memberId, request, request.licenseImage());
 
     return ApiResponse.onSuccess(SuccessStatus.SELLER_REGISTER_SUCCESS);
   }
