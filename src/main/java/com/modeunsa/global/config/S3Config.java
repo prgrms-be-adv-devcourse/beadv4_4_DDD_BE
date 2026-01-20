e71076395de609ed97e6df06454c52c2c1936ac7
sohee52 <wneh2023@naver.com>
2026-01-20 09:57:25 +0900

    [#182] feat: S3Config 설정 클래스 추가

---
+++ src/main/java/com/modeunsa/global/config/S3Config.java
@@ -0,0 +1,31 @@
+package com.modeunsa.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@EnableConfigurationProperties(S3Properties.class)
+@RequiredArgsConstructor
+public class S3Config {
+
+  private final S3Properties s3Properties;
+
+  @Bean
+  public S3Client s3Client() {
+    AwsBasicCredentials credentials = AwsBasicCredentials.create(
+        s3Properties.credentials().accessKey(),
+        s3Properties.credentials().secretKey()
+    );
+
+    return S3Client.builder()
+        .region(Region.of(s3Properties.region().staticRegion()))
+        .credentialsProvider(StaticCredentialsProvider.create(credentials))
+        .build();
+  }
+}
