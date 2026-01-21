9e0c2e0dc6df4a369f27e8837dec1112728dfa3d
sohee52 <wneh2023@naver.com>
2026-01-21 12:50:21 +0900

    [#19] refactor: return 타입 void로 변경

--- src/main/java/com/modeunsa/boundedcontext/member/domain/entity/Member.java
+++ src/main/java/com/modeunsa/boundedcontext/member/domain/entity/Member.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -59,7 +60,7 @@ public class Member extends GeneratedIdAndAuditedEntity {
   @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
   private List<OAuthAccount> oauthAccount = new ArrayList<>();
 
-  @OneToOne(mappedBy = "member", cascade = CascadeType.ALL)
+  @OneToOne(mappedBy = "member", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private MemberProfile profile;
 
   @Getter(AccessLevel.NONE)
@@ -127,11 +128,10 @@ public class Member extends GeneratedIdAndAuditedEntity {
     return this;
   }
 
-  public Member updateEmail(String email) {
+  public void updateEmail(String email) {
     if (email != null) {
       this.email = email;
     }
-    return this;
   }
 
   public void changeRole(MemberRole role) {
