9ce971aa4875fccd7fa9e13880ee8a1adf194fbb
kjuiop <copying_y@naver.com>
2026-01-21 10:00:47 +0900

    [#209] fix: 기존 트랜잭션 제거

--- src/main/java/com/modeunsa/boundedcontext/payment/app/support/PaymentAccountSupport.java
+++ src/main/java/com/modeunsa/boundedcontext/payment/app/support/PaymentAccountSupport.java
@@ -8,10 +8,8 @@ import com.modeunsa.global.exception.GeneralException;
 import com.modeunsa.global.status.ErrorStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PaymentAccountSupport {
 
