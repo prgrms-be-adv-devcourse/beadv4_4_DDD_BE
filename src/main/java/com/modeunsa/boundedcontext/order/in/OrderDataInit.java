492222124aff8d88b5d8a1b749324cfcd205860f
OneE <bestoneeee@naver.com>
2026-01-21 16:27:20 +0900

    [#196] chore: test 에러 수정

--- src/main/java/com/modeunsa/boundedcontext/order/in/OrderDataInit.java
+++ src/main/java/com/modeunsa/boundedcontext/order/in/OrderDataInit.java
@@ -13,9 +13,11 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
+@Profile({"local", "dev"})
 @Slf4j
 public class OrderDataInit {
 
