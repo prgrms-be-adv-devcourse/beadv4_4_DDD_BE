46b282ac48ede6bccaded2c9fe7c74ec574d6a10
JeongIn <41246605+kjuiop@users.noreply.github.com>
2026-01-21 10:02:47 +0900

    [FEAT] Data Init 부분에서 기존 데이터가 존재 시 skip 하도록 한다. (#209)
    * [#209] feat: data init 시 validation 추가
    
    * [#209] fix: 기존 트랜잭션 제거

--- src/main/java/com/modeunsa/boundedcontext/payment/app/support/PaymentAccountSupport.java
+++ src/main/java/com/modeunsa/boundedcontext/payment/app/support/PaymentAccountSupport.java
@@ -55,4 +55,8 @@ public class PaymentAccountSupport {
         .findByMemberIdWithLock(holderMemberId)
         .orElseThrow(() -> new GeneralException(ErrorStatus.PAYMENT_ACCOUNT_NOT_FOUND));
   }
+
+  public long countMemberAccount() {
+    return paymentAccountRepository.count();
+  }
 }
