545d55ceb1cf2f5a1184cd99ba97d7516cf86ff6
kjuiop <copying_y@naver.com>
2026-01-23 00:15:22 +0900

    [#246] feat: 매거진에서 글쓰기 버튼 추가

--- web-client/app/magazine/page.tsx
+++ web-client/app/magazine/page.tsx
@@ -157,6 +157,14 @@ export default function MagazinePage() {
         </div>
       </section>
 
+      {/* Write Button */}
+      <Link href="/magazine/write" className="magazine-write-button">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+        <span>글쓰기</span>
+      </Link>
+
       {/* Footer */}
       <footer className="footer">
         <div className="container">
