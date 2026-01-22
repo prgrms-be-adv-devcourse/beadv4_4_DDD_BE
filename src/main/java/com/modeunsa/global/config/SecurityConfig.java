ea90dca52edc70c68266b6db5df920f6aee9dd50
sohee52 <wneh2023@naver.com>
2026-01-22 12:50:56 +0900

    [#238] style: 컨벤션에 따라 코드 스타일 변경

--- src/main/java/com/modeunsa/global/config/SecurityConfig.java
+++ src/main/java/com/modeunsa/global/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -38,11 +37,7 @@ public class SecurityConfig {
       String[] permittedUrls = securityProperties.getPermitUrls().toArray(new String[0]);
 
       http.authorizeHttpRequests(
-          auth ->
-              auth.requestMatchers(permittedUrls)
-                  .permitAll()
-                  .anyRequest()
-                  .authenticated());
+          auth -> auth.requestMatchers(permittedUrls).permitAll().anyRequest().authenticated());
     }
 
     http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
