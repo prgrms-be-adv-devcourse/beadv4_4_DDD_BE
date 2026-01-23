57e6cde4e6ec0a85eb7b4c635def133570ccb893
sohee52 <wneh2023@naver.com>
2026-01-23 15:31:47 +0900

    [#205] refactor: 콘텐츠 타입 필드 추가, message 추가

--- src/main/java/com/modeunsa/shared/member/dto/request/SellerRegisterRequest.java
+++ src/main/java/com/modeunsa/shared/member/dto/request/SellerRegisterRequest.java
@@ -10,7 +10,7 @@ public record SellerRegisterRequest(
     @NotBlank(message = "계좌번호는 필수입니다.")
         @Pattern(regexp = BANK_ACCOUNT_REGEX, message = "계좌번호 형식이 올바르지 않습니다")
         String settlementBankAccount,
-    @NotBlank String licenseImage,
-    @NotBlank String licenseContentType) {
+    @NotBlank(message = "사업자등록증 이미지 키는 필수입니다.") String licenseImageRawKey,
+    @NotBlank(message = "콘텐츠 타입은 필수입니다.") String licenseContentType) {
   public static final String BANK_ACCOUNT_REGEX = "^[0-9-]{10,20}$";
 }
