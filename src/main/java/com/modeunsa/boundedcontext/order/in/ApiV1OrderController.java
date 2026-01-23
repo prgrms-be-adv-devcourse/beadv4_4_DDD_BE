334eb8bf0ce634c414a3543f02aeeebc3374122b
OneE <bestoneeee@naver.com>
2026-01-23 15:09:26 +0900

    [#268] feat: 모든 api에 AuthenticationPrincipal추가

--- src/main/java/com/modeunsa/boundedcontext/order/in/ApiV1OrderController.java
+++ src/main/java/com/modeunsa/boundedcontext/order/in/ApiV1OrderController.java
@@ -21,6 +21,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,10 +40,8 @@ public class ApiV1OrderController {
   @Operation(summary = "장바구니에 상품 추가 기능", description = "장바구니에 상품을 추가하는 기능입니다.")
   @PostMapping("/cart/item")
   public ResponseEntity<ApiResponse> createCartItem(
-      // @AuthenticationPrincipal Long memberId // 나중에 시큐리티 적용 시
+      @AuthenticationPrincipal Long memberId,
       @RequestBody @Valid CreateCartItemRequestDto requestDto) {
-    // [TODO] 실제 로그인한 유저 ID를 가져오는 로직 추가
-    long memberId = 1;
 
     CreateCartItemResponseDto dto = orderFacade.createCartItem(memberId, requestDto);
 
@@ -52,10 +51,8 @@ public class ApiV1OrderController {
   @Operation(summary = "단건 주문 생성 기능", description = "단건 상품을 주문하는 기능입니다.")
   @PostMapping
   public ResponseEntity<ApiResponse> createOrder(
-      // @AuthenticationPrincipal Long memberId // 나중에 시큐리티 적용 시
+      @AuthenticationPrincipal Long memberId,
       @RequestBody @Valid CreateOrderRequestDto requestDto) {
-    // [TODO] 실제 로그인한 유저 ID를 가져오는 로직 추가
-    long memberId = 1;
 
     OrderResponseDto dto = orderFacade.createOrder(memberId, requestDto);
 
@@ -65,10 +62,8 @@ public class ApiV1OrderController {
   @Operation(summary = "장바구니 주문 생성 기능", description = "장바구니에 있는 모든 상품을 주문하는 기능입니다.")
   @PostMapping("/cart-order")
   public ResponseEntity<ApiResponse> createCartOrder(
-      // @AuthenticationPrincipal Long memberId // 나중에 시큐리티 적용 시
+      @AuthenticationPrincipal Long memberId,
       @RequestBody @Valid CreateCartOrderRequestDto requestDto) {
-    // [TODO] 실제 로그인한 유저 ID를 가져오는 로직 추가
-    long memberId = 1;
 
     OrderResponseDto dto = orderFacade.createCartOrder(memberId, requestDto);
 
@@ -78,12 +73,10 @@ public class ApiV1OrderController {
   @Operation(summary = "주문 목록 조회 기능", description = "생성한 주문들의 목록을 확인할 수 있는 기능입니다.")
   @GetMapping
   public ResponseEntity<ApiResponse> getOrders(
-      // @AuthenticationPrincipal Long memberId // 나중에 시큐리티 적용 시
+      @AuthenticationPrincipal Long memberId,
       @ParameterObject
           @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
           Pageable pageable) {
-    // [TODO] 실제 로그인한 유저 ID를 가져오는 로직 추가
-    long memberId = 1;
 
     Page<OrderListResponseDto> pageDto = orderFacade.getOrders(memberId, pageable);
 
@@ -93,10 +86,7 @@ public class ApiV1OrderController {
   @Operation(summary = "회원 주문 취소 요청 기능", description = "회원이 배송 전 상태인 주문을 취소 요청할 수 있는 기능입니다.")
   @PostMapping("/{orderId}/cancel")
   public ResponseEntity<ApiResponse> cancelOrder(
-      // @AuthenticationPrincipal Long memberId // 나중에 시큐리티 적용 시
-      @PathVariable Long orderId) {
-    // [TODO] 실제 로그인한 유저 ID를 가져오는 로직 추가
-    long memberId = 1;
+      @AuthenticationPrincipal Long memberId, @PathVariable Long orderId) {
 
     OrderResponseDto dto = orderFacade.cancelOrder(memberId, orderId);
 
@@ -106,10 +96,7 @@ public class ApiV1OrderController {
   @Operation(summary = "회원 주문 환불 요청 기능", description = "배송 완료된 주문을 환불 요청할 수 있는 기능입니다.")
   @PostMapping("/{orderId}/refund")
   public ResponseEntity<ApiResponse> refundOrder(
-      // @AuthenticationPrincipal Long memberId // 나중에 시큐리티 적용 시
-      @PathVariable Long orderId) {
-    // [TODO] 실제 로그인한 유저 ID를 가져오는 로직 추가
-    long memberId = 1;
+      @AuthenticationPrincipal Long memberId, @PathVariable Long orderId) {
 
     OrderResponseDto dto = orderFacade.refundOrder(memberId, orderId);
 
@@ -118,13 +105,7 @@ public class ApiV1OrderController {
 
   @Operation(summary = "장바구니 상품 목록 조회 기능", description = "장바구니 상품 목록을 확인할 수 있는 기능입니다.")
   @GetMapping("/cart-items")
-  public ResponseEntity<ApiResponse> getCartItem(
-      // @AuthenticationPrincipal Long memberId // 나중에 시큐리티 적용 시
-      @ParameterObject
-          @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC)
-          Pageable pageable) {
-    // [TODO] 실제 로그인한 유저 ID를 가져오는 로직 추가
-    long memberId = 1;
+  public ResponseEntity<ApiResponse> getCartItem(@AuthenticationPrincipal Long memberId) {
 
     CartItemsResponseDto dto = orderFacade.getCartItems(memberId);
 
