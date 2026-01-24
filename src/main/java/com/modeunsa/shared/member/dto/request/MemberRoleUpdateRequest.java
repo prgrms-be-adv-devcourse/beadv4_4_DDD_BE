cfed9f623bb01735718d4a2b082da4da4e8388a2
sohee52 <wneh2023@naver.com>
2026-01-24 09:33:33 +0900

    [#17] refactor: MemberRoleUpdateRequest에 @NotNull 추가

--- src/main/java/com/modeunsa/shared/member/dto/request/MemberRoleUpdateRequest.java
+++ src/main/java/com/modeunsa/shared/member/dto/request/MemberRoleUpdateRequest.java
@@ -1,5 +1,6 @@
 package com.modeunsa.shared.member.dto.request;
 
 import com.modeunsa.boundedcontext.member.domain.types.MemberRole;
+import jakarta.validation.constraints.NotNull;
 
-public record MemberRoleUpdateRequest(MemberRole role) {}
+public record MemberRoleUpdateRequest(@NotNull MemberRole role) {}
