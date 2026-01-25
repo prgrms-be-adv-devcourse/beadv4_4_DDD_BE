c9c520e542d21cc3b36cb1771dd9c9d5a4101269
sohee52 <wneh2023@naver.com>
2026-01-25 23:58:37 +0900

    [#289] fix: 중복 선언된 CorsProperties 제거

--- src/main/java/com/modeunsa/global/config/SecurityProperties.java
+++ src/main/java/com/modeunsa/global/config/SecurityProperties.java
@@ -14,18 +14,10 @@ public class SecurityProperties {
   private boolean permitAll;
   private List<String> permitUrls = new ArrayList<>();
   private OAuth2Properties oauth2 = new OAuth2Properties();
-  private CorsProperties cors = new CorsProperties();
 
   @Getter
   @Setter
   public static class OAuth2Properties {
     private List<String> allowedRedirectDomains = new ArrayList<>();
-  }
-
-  @Getter
-  @Setter
-  public static class CorsProperties {
-    private boolean enabled;
-    private List<String> allowedOrigins = new ArrayList<>();
   }
 }
