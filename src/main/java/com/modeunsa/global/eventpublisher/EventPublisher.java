94303bb0a740b2be6d2c64f2095bb32c6bfe9a22
kjuiop <copying_y@naver.com>
2026-01-30 16:42:35 +0900

    [#314] docs: envetPublisher 에 주석 추가

--- src/main/java/com/modeunsa/global/eventpublisher/EventPublisher.java
+++ src/main/java/com/modeunsa/global/eventpublisher/EventPublisher.java
@@ -1,5 +1,15 @@
 package com.modeunsa.global.eventpublisher;
 
+/**
+ * 도메인 이벤트를 발행하는 인터페이스. Spring 내부 이벤트 또는 Kafka와 같은 외부 메시징 시스템으로 발행할 수 있습니다.
+ *
+ * <p>구현체 예시:
+ *
+ * <ul>
+ *   <li>SpringDomainEventPublisher: Spring의 {@code ApplicationEventPublisher} 사용
+ *   <li>KafkaDomainEventPublisher: Kafka로 이벤트 발행
+ * </ul>
+ */
 public interface EventPublisher {
   void publish(Object event);
 }
