6823941ef6a5dc5806bb7c2bb99d7c9dce19efb4
OneE <bestoneeee@naver.com>
2026-01-30 15:46:00 +0900

    [#312] chore: 판매자 정보 암호화 해제

--- src/main/java/com/modeunsa/boundedcontext/inventory/domain/InventorySeller.java
+++ src/main/java/com/modeunsa/boundedcontext/inventory/domain/InventorySeller.java
@@ -1,9 +1,7 @@
 package com.modeunsa.boundedcontext.inventory.domain;
 
-import com.modeunsa.global.jpa.converter.EncryptedStringConverter;
 import com.modeunsa.global.jpa.entity.ManualIdAndAuditedEntity;
 import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -19,11 +17,10 @@ import lombok.experimental.SuperBuilder;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class InventorySeller extends ManualIdAndAuditedEntity {
-  @Convert(converter = EncryptedStringConverter.class)
+
   @Column(name = "business_name", nullable = false, length = 500)
   private String businessName;
 
-  @Convert(converter = EncryptedStringConverter.class)
   @Column(name = "representative_name", nullable = false, length = 500)
   private String representativeName;
 }
