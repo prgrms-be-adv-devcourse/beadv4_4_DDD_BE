9f63ce849ffe99efd5ba6afaa70f7380f8286394
kjuiop <copying_y@naver.com>
2026-02-01 00:58:59 +0900

    [#358] feat: 회원 가입 이벤트를 kafka 로 연동

---
+++ src/main/java/com/modeunsa/boundedcontext/payment/in/PaymentKafkaEventListener.java
@@ -0,0 +1,39 @@
+package com.modeunsa.boundedcontext.payment.in;
+
+import static org.springframework.transaction.annotation.Propagation.REQUIRES_NEW;
+
+import com.modeunsa.boundedcontext.payment.app.PaymentFacade;
+import com.modeunsa.boundedcontext.payment.app.dto.member.PaymentMemberDto;
+import com.modeunsa.boundedcontext.payment.app.mapper.PaymentMapper;
+import com.modeunsa.global.eventpublisher.topic.DomainEventEnvelope;
+import com.modeunsa.shared.member.event.MemberSignupEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import tools.jackson.databind.ObjectMapper;
+
+@Component
+@RequiredArgsConstructor
+public class PaymentKafkaEventListener {
+
+  private final PaymentFacade paymentFacade;
+  private final PaymentMapper paymentMapper;
+  private final ObjectMapper objectMapper;
+
+  @KafkaListener(topics = "member-events", groupId = "payment-service")
+  @Transactional(propagation = REQUIRES_NEW)
+  public void handleMemberCreateEvent(DomainEventEnvelope envelope) {
+    switch (envelope.eventType()) {
+      case "MemberSignupEvent" -> {
+        MemberSignupEvent event =
+            objectMapper.readValue(envelope.payload(), MemberSignupEvent.class);
+        PaymentMemberDto member = paymentMapper.toPaymentMemberDto(event);
+        paymentFacade.createPaymentMember(member);
+      }
+      default -> {
+        // ignore}
+      }
+    }
+  }
+}
