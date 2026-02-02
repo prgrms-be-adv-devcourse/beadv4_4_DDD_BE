7061686fe81591650156d9b5b31b901ce6853d8d
WONHEE JEONG <bestoneeee@naver.com>
2026-02-02 14:22:03 +0900

    [FEAT] #318: 판매자 재고 수정 기능 (#388)
    * [#318] feat: 재고 수정 기능
    
    * [#318] feat: 재고 수정 도메인 메서드 테스트
    
    * [#318] chore: 재고수량 0가능하게 수정
    
    * [#318] chore: 데이터베이스명 변경
    
    * [#318] feat: 재고 품절 이벤트 발행
    
    * [#318] chore: swagger설명 수정
    
    * [#318] feat: RequestMapping추가 및 컨트롤러 버전 추가
    
    * [#318] chore: 파일 위치 변경
    
    * [#318] feat: 도메인메서드 추가 및 수정로직 재시도 처리
    
    * [#318] feat: 재고수정 업데이트 테스트
    
    * [#318] chore: 동시성 테스트 빌드 제외

---
+++ src/main/java/com/modeunsa/boundedcontext/inventory/in/api/v2/InventoryController.java
@@ -0,0 +1,41 @@
+package com.modeunsa.boundedcontext.inventory.in.api.v2;
+
+import com.modeunsa.boundedcontext.inventory.app.InventoryFacade;
+import com.modeunsa.global.response.ApiResponse;
+import com.modeunsa.global.security.CustomUserDetails;
+import com.modeunsa.global.status.SuccessStatus;
+import com.modeunsa.shared.inventory.dto.InventoryUpdateRequest;
+import com.modeunsa.shared.inventory.dto.InventoryUpdateResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/inventories")
+@Tag(name = "Inventory", description = "재고 도메인 API")
+public class InventoryController {
+  private final InventoryFacade inventoryFacade;
+
+  @Operation(summary = "실재고 수정", description = "판매자가 상품 실재고를 수정합니다.")
+  @PatchMapping("/{productId}")
+  public ResponseEntity<ApiResponse> updateInventory(
+      @AuthenticationPrincipal CustomUserDetails user,
+      Long productId,
+      @Valid @RequestBody InventoryUpdateRequest inventoryUpdateRequest) {
+
+    // TODO: CustomUserDetails에서 sellerId가져오기
+    Long sellerId = 1L;
+    InventoryUpdateResponse response =
+        inventoryFacade.updateInventory(sellerId, productId, inventoryUpdateRequest);
+
+    return ApiResponse.onSuccess(SuccessStatus.OK, response);
+  }
+}
