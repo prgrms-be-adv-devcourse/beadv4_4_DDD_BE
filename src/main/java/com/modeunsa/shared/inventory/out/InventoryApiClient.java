cbeb0b1b5e226c06cac2e4eb59b488e40869fe6c
OneE <bestoneeee@naver.com>
2026-02-02 15:14:39 +0900

    [#316] feat: inventoryApiClient추가

---
+++ src/main/java/com/modeunsa/shared/inventory/out/InventoryApiClient.java
@@ -0,0 +1,29 @@
+package com.modeunsa.shared.inventory.out;
+
+import com.modeunsa.shared.inventory.dto.InventoryDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+@Service
+public class InventoryApiClient {
+  private final RestClient restClient;
+
+  public InventoryApiClient(
+      @Value("${custom.global.internalBackUrl}") String internalBackUrl,
+      @Value("${internal.api-key}") String internalApiKey) {
+    this.restClient =
+        RestClient.builder()
+            .baseUrl(internalBackUrl + "/api/v1/inventories")
+            .defaultHeader("X-INTERNAL-API-KEY", internalApiKey)
+            .build();
+  }
+
+  public InventoryDto getInventory(Long productId) {
+    return restClient
+        .get()
+        .uri("/internal/{productId}", productId)
+        .retrieve()
+        .body(InventoryDto.class);
+  }
+}
