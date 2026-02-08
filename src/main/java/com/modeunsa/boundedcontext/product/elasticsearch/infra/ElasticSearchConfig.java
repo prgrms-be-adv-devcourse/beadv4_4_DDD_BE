700fde5a080ab7d0e1d6f20ab4b6dd6c47f5c9b8
ChanHee Park <59467631+chanheess@users.noreply.github.com>
2026-02-08 22:26:22 +0900

    [#461] feat: 각 환경 분리

--- src/main/java/com/modeunsa/boundedcontext/product/elasticsearch/infra/ElasticSearchConfig.java
+++ src/main/java/com/modeunsa/boundedcontext/product/elasticsearch/infra/ElasticSearchConfig.java
@@ -4,17 +4,23 @@ import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import java.net.URI;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ElasticSearchConfig {
 
+  @Value("${spring.elasticsearch.uris:http://localhost:9200}")
+  private String elasticsearchUri;
+
   @Bean
   public RestClient restClient() {
-    return RestClient.builder(new HttpHost("localhost", 9200, "http")).build();
+    URI uri = URI.create(elasticsearchUri.split(",")[0].trim());
+    return RestClient.builder(new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme())).build();
   }
 
   @Bean
