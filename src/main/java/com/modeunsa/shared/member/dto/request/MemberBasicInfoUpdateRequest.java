8b64d0eb7f707fde2a2e9da0070c0e2c3b54efea
sohee52 <wneh2023@naver.com>
2026-02-09 19:15:10 +0900

    [#439] feat: MemberBasicInfoUpdateRequest에 @AllArgsConstructor 추가

--- src/main/java/com/modeunsa/shared/member/dto/request/MemberBasicInfoUpdateRequest.java
+++ src/main/java/com/modeunsa/shared/member/dto/request/MemberBasicInfoUpdateRequest.java
@@ -2,11 +2,13 @@ package com.modeunsa.shared.member.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MemberBasicInfoUpdateRequest {
   private String realName;
 
