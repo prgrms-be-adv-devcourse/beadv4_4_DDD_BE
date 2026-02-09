9bf438c187aa24dd0786b4452daa7be8cd576ea8
OneE <bestoneeee@naver.com>
2026-02-09 17:19:41 +0900

    [Refactor] #441: 주문 배송 정보 입력 기능 추가 (#478)
    * [#321] refactor: 주문요청dto에서 배송정보 삭제
    
    * [#441] refactor: 주문배송정보 입력 기능 추가

--- src/main/java/com/modeunsa/boundedcontext/order/in/api/v2/OrderController.java
+++ src/main/java/com/modeunsa/boundedcontext/order/in/api/v2/OrderController.java
@@ -4,9 +4,11 @@ import com.modeunsa.boundedcontext.order.app.OrderFacade;
 import com.modeunsa.global.response.ApiResponse;
 import com.modeunsa.global.security.CustomUserDetails;
 import com.modeunsa.global.status.SuccessStatus;
+import com.modeunsa.shared.order.OrderDeliveryRequestDto;
 import com.modeunsa.shared.order.dto.DeleteCartItemsRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,7 +42,7 @@ public class OrderController {
   @DeleteMapping("/cart-items")
   public ResponseEntity<ApiResponse> deleteCartItems(
       @AuthenticationPrincipal CustomUserDetails user,
-      @RequestBody DeleteCartItemsRequestDto request) {
+      @RequestBody @Valid DeleteCartItemsRequestDto request) {
     Long memberId = user.getMemberId();
     orderFacade.deleteCartItems(memberId, request);
     return ApiResponse.onSuccess(SuccessStatus.NO_CONTENT);
@@ -51,6 +54,17 @@ public class OrderController {
       @AuthenticationPrincipal CustomUserDetails user) {
     Long memberId = user.getMemberId();
     orderFacade.deleteAllCartItems(memberId);
+    return ApiResponse.onSuccess(SuccessStatus.NO_CONTENT);
+  }
+
+  @Operation(summary = "배송정보 입력 기능", description = "주문배송정보를 입력하는 기능입니다.")
+  @PostMapping("/delivery-info")
+  public ResponseEntity<ApiResponse> addOrderDeliveryInfo(
+      @AuthenticationPrincipal CustomUserDetails user,
+      Long orderId,
+      @RequestBody @Valid OrderDeliveryRequestDto orderDeliveryRequestDto) {
+    Long memberId = user.getMemberId();
+    orderFacade.addOrderDeliveryInfo(memberId, orderId, orderDeliveryRequestDto);
     return ApiResponse.onSuccess(SuccessStatus.NO_CONTENT);
   }
 }
