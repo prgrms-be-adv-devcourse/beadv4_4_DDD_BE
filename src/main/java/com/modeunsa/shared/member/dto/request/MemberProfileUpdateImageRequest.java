c2acfb3f51a3789ccbbee56f689c27a155a85975
sohee52 <wneh2023@naver.com>
2026-02-12 12:18:16 +0900

    [#431] feat: 이미지 URL을 받기 위한 MemberProfileUpdateImageRequest 추가

---
+++ src/main/java/com/modeunsa/shared/member/dto/request/MemberProfileUpdateImageRequest.java
@@ -0,0 +1,7 @@
+package com.modeunsa.shared.member.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record MemberProfileUpdateImageRequest(
+    @NotBlank(message = "이미지 URL은 필수입니다.") String imageUrl
+) {}
