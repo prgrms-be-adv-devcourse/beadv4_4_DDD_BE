82b8a580507ead3f816bb1d6e2b2e9399f7410bc
rkdls8268 <rkdls8268@gmail.com>
2026-02-12 23:08:53 +0900

    [#503] chore: productInternalBackUrl 로 변경

--- api/src/main/java/com/modeunsa/shared/product/out/ProductApiClient.java
+++ api/src/main/java/com/modeunsa/shared/product/out/ProductApiClient.java
@@ -3,24 +3,22 @@ package com.modeunsa.shared.product.out;
 import com.modeunsa.shared.product.dto.ProductOrderResponse;
 import com.modeunsa.shared.product.dto.ProductOrderValidateRequest;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
-@Slf4j
 @Service
 public class ProductApiClient {
 
   private final RestClient restClient;
 
   public ProductApiClient(
-      @Value("${custom.global.productInternalBackUrl}") String internalBackUrl,
+      @Value("${custom.global.productInternalBackUrl}") String productInternalBackUrl,
       @Value("${internal.api-key}") String internalApiKey) {
     this.restClient =
         RestClient.builder()
-            .baseUrl(internalBackUrl + "/api/v1/products")
+            .baseUrl(productInternalBackUrl + "/api/v1/products")
             .defaultHeader("X-INTERNAL-API-KEY", internalApiKey)
             .build();
   }
