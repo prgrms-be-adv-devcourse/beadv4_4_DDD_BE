eb56ecdfba72e106d72b30c6c47cdccce2dafc23
sohee52 <wneh2023@naver.com>
2026-02-12 15:51:10 +0900

    [#326] style: 컨벤션에 맞게 코드 스타일 수정

--- src/main/java/com/modeunsa/shared/member/dto/response/MemberBasicInfoResponse.java
+++ src/main/java/com/modeunsa/shared/member/dto/response/MemberBasicInfoResponse.java
@@ -4,7 +4,8 @@ import com.modeunsa.boundedcontext.member.domain.entity.Member;
 import lombok.Builder;
 
 @Builder
-public record MemberBasicInfoResponse(Long id, String email, String realName, String phoneNumber, String role) {
+public record MemberBasicInfoResponse(
+    Long id, String email, String realName, String phoneNumber, String role) {
   public static MemberBasicInfoResponse from(Member member) {
     return MemberBasicInfoResponse.builder()
         .id(member.getId())
