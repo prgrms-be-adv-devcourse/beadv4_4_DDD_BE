be061206d660c065b765d99cdab89a328e980a16
Sohee Kim <wneh2023@naver.com>
2026-02-13 17:35:09 +0900

    [Feat] #476 판매자 등록 화면 연동 (#558)
    * [#326] feat: 판매자 등록 API를 화면과 연동
    
    * [#476] refactor: 바뀐 role이 포함된 쿠키를 함께 반환하도록 백엔드 로직 수정
    
    * [#476] chore: 파일 경로 주석 추가
    
    * [#476] feat: seller-request 페이지를 백엔드와 연동
    
    * [#476] refactor: 반응형 데이터 갱신을 위해 마이페이지 수정
    
    * [#476] style: 컨벤션에 맞게 코드 스타일 수정
    
    * [#476] refactor: 불완전한 확장자 대비 로직 추가
    
    * [#476] refactor: 사용 안 하는 변수와 import 제거
    
    * [#476] refactor: 확장자 통일, 파일 크기 제한 로직 추가

--- member/src/main/java/com/modeunsa/boundedcontext/member/app/facade/MemberSellerFacade.java
+++ member/src/main/java/com/modeunsa/boundedcontext/member/app/facade/MemberSellerFacade.java
@@ -1,13 +1,10 @@
 package com.modeunsa.boundedcontext.member.app.facade;
 
-import com.modeunsa.boundedcontext.member.app.support.MemberSupport;
 import com.modeunsa.boundedcontext.member.app.usecase.SellerInfoGetUseCase;
 import com.modeunsa.boundedcontext.member.app.usecase.SellerRegisterUseCase;
-import com.modeunsa.boundedcontext.member.domain.entity.Member;
-import com.modeunsa.global.security.jwt.JwtTokenProvider;
+import com.modeunsa.shared.auth.dto.JwtTokenResponse;
 import com.modeunsa.shared.member.dto.request.SellerRegisterRequest;
 import com.modeunsa.shared.member.dto.response.SellerInfoResponse;
-import com.modeunsa.shared.member.dto.response.SellerRegisterResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,26 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberSellerFacade {
   private final SellerRegisterUseCase sellerRegisterUseCase;
-  private final JwtTokenProvider jwtTokenProvider;
   private final SellerInfoGetUseCase sellerInfoGetUseCase;
-  private final MemberSupport memberSupport;
 
   /** 판매자 등록 */
   @Transactional
-  public SellerRegisterResponse registerSeller(Long memberId, SellerRegisterRequest request) {
-    sellerRegisterUseCase.execute(memberId, request, request.businessLicenseUrl());
-
-    Member member = memberSupport.getMember(memberId);
-    Long sellerId = memberSupport.getSellerIdByMemberId(memberId);
-
-    String accessToken =
-        jwtTokenProvider.createAccessToken(
-            member.getId(), member.getRole(), sellerId, member.getStatus().name());
-    String refreshToken =
-        jwtTokenProvider.createRefreshToken(
-            member.getId(), member.getRole(), sellerId, member.getStatus().name());
-
-    return new SellerRegisterResponse(accessToken, refreshToken);
+  public JwtTokenResponse registerSeller(Long memberId, SellerRegisterRequest request) {
+    return sellerRegisterUseCase.execute(memberId, request, request.businessLicenseUrl());
   }
 
   /** 판매자 정보 조회 */
