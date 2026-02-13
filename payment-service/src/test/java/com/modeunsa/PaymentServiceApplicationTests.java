4bfa56765b762a1b8626fa96ee94c6cce1ace948
kjuiop <copying_y@naver.com>
2026-02-13 15:27:33 +0900

    [#315] fix: unused import remove

--- payment-service/src/test/java/com/modeunsa/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/modeunsa/PaymentServiceApplicationTests.java
@@ -1,9 +1,7 @@
 package com.modeunsa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ActiveProfiles("test")
 public class PaymentServiceApplicationTests {
