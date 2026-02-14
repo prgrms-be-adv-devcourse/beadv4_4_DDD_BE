1ae338ff62730f1fe4f6a921835ea3514ab490f3
kjuiop <copying_y@naver.com>
2026-02-14 15:54:46 +0900

    [#566] feat: outbox publish 기능 추가

---
+++ common/src/main/java/com/modeunsa/global/eventpublisher/topic/KafkaTopics.java
@@ -0,0 +1,9 @@
+package com.modeunsa.global.eventpublisher.topic;
+
+public class KafkaTopics {
+  public static final String PAYMENT_MEMBER_CREATED = "payment.member.created";
+  public static final String PAYMENT_SUCCESS = "payment.success";
+  public static final String PAYMENT_FAILED = "payment.failed";
+  public static final String PAYMENT_FINAL_FAILED = "payment.final.failed";
+  public static final String PAYMENT_REFUND_SUCCESS = "payment.refund.success";
+}
