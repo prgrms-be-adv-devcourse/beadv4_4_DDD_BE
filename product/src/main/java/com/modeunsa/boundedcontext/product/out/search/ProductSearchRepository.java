01009fe9770db97f6fd2d4feda969c0b06d74511
rkdls8268 <rkdls8268@gmail.com>
2026-02-16 22:43:37 +0900

    [#99] fix: 불필요한 es 관련 설정 제거

--- product/src/main/java/com/modeunsa/boundedcontext/product/out/search/ProductSearchRepository.java
+++ product/src/main/java/com/modeunsa/boundedcontext/product/out/search/ProductSearchRepository.java
@@ -2,8 +2,10 @@ package com.modeunsa.boundedcontext.product.out.search;
 
 import com.modeunsa.boundedcontext.product.domain.search.document.ProductSearch;
 import java.util.List;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+@ConditionalOnProperty(name = "app.elasticsearch.enabled", havingValue = "true")
 public interface ProductSearchRepository extends ElasticsearchRepository<ProductSearch, String> {
   List<ProductSearch> findAll();
 
