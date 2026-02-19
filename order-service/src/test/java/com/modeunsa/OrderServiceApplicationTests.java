34cdd6f98871571d07cc05e185af2fb0f326d91b
OneE <bestoneeee@naver.com>
2026-02-19 15:35:03 +0900

    [#526] chore: 테스트 프로필설정

--- order-service/src/test/java/com/modeunsa/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/modeunsa/OrderServiceApplicationTests.java
@@ -1,9 +1,9 @@
 package com.modeunsa;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = OrderServiceApplication.class)
+@ActiveProfiles("test")
 class OrderServiceApplicationTests {
 
   @Test
