faca57d2e3f2339ce7422c0d560d89d348ba4620
sohee52 <wneh2023@naver.com>
2026-02-20 12:43:00 +0900

    [#423] refactor(file): 공통 헤더 기반 시큐리티 적용 및 엔드포인트 권한 재설정

---
+++ file/src/main/java/com/modeunsa/global/config/SecurityProperties.java
@@ -0,0 +1,23 @@
+package com.modeunsa.global.config;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "custom.security")
+public class SecurityProperties {
+
+  private boolean permitAll;
+  private List<String> permitUrls = new ArrayList<>();
+  private OAuth2Properties oauth2 = new OAuth2Properties();
+
+  @Getter
+  @Setter
+  public static class OAuth2Properties {
+    private List<String> allowedRedirectDomains = new ArrayList<>();
+  }
+}
