c222346db36f8c0e6abcb63d4e7af81f875d2a7a
sohee52 <wneh2023@naver.com>
2026-02-20 12:43:42 +0900

    [#423] refactor(file): SecurityProperties에서 필요없는 필드 제거

--- file/src/main/java/com/modeunsa/global/config/SecurityProperties.java
+++ file/src/main/java/com/modeunsa/global/config/SecurityProperties.java
@@ -13,11 +13,4 @@ public class SecurityProperties {
 
   private boolean permitAll;
   private List<String> permitUrls = new ArrayList<>();
-  private OAuth2Properties oauth2 = new OAuth2Properties();
-
-  @Getter
-  @Setter
-  public static class OAuth2Properties {
-    private List<String> allowedRedirectDomains = new ArrayList<>();
-  }
 }
