21ff636959347a2ff72bf71ffe51d35849a68d84
sohee52 <wneh2023@naver.com>
2026-02-22 22:04:55 +0900

    [#621] refactor(member): 공통 메서드인 roleHierarchy 제거

--- member/src/main/java/com/modeunsa/global/config/SecurityConfig.java
+++ member/src/main/java/com/modeunsa/global/config/SecurityConfig.java
@@ -9,8 +9,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -101,20 +99,5 @@ public class SecurityConfig {
     http.addFilterBefore(internalApiKeyFilter, UsernamePasswordAuthenticationFilter.class);
     http.addFilterBefore(jwtAuthenticationFilter, InternalApiKeyFilter.class);
     return http.build();
-  }
-
-  /** Role Hierarchy 설정 */
-  @Bean
-  public RoleHierarchy roleHierarchy() {
-    return RoleHierarchyImpl.withDefaultRolePrefix()
-        .role("SYSTEM")
-        .implies("ADMIN") // SYSTEM은 ADMIN의 모든 권한을 가짐
-        .role("HOLDER")
-        .implies("ADMIN") // HOLDER는 ADMIN의 모든 권한을 가짐
-        .role("ADMIN")
-        .implies("SELLER") // ADMIN은 SELLER의 모든 권한을 가짐
-        .role("SELLER")
-        .implies("MEMBER") // SELLER는 MEMBER의 모든 권한을 가짐
-        .build();
   }
 }
