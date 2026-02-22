2ae4373159c6e41dd6fc4c9dc3b6b294ef948513
sohee52 <wneh2023@naver.com>
2026-02-22 21:42:30 +0900

    [#621] refactor(common): 공통 메서드 roleHierarchy()를 CommonSecurityConfig로 생성

---
+++ common/src/main/java/com/modeunsa/global/config/CommonSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.modeunsa.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+
+@Configuration
+public class CommonSecurityConfig {
+
+  @Bean
+  public RoleHierarchy roleHierarchy() {
+    return RoleHierarchyImpl.withDefaultRolePrefix()
+        .role("SYSTEM").implies("ADMIN")
+        .role("HOLDER").implies("ADMIN")
+        .role("ADMIN").implies("SELLER")
+        .role("SELLER").implies("MEMBER")
+        .build();
+  }
+}
