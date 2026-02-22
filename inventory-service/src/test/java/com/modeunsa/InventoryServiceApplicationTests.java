a31af12858fc9482049121f5e8459c747a862f64
OneE <bestoneeee@naver.com>
2026-02-22 20:13:40 +0900

    [#527] chore: application설정

--- inventory-service/src/test/java/com/modeunsa/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/modeunsa/InventoryServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.modeunsa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = InventoryServiceApplication.class)
 class InventoryServiceApplicationTests {
 
   @Test
