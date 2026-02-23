eb18cdbb9d1dd2b077537a35256e1e573b7a03fc
sohee52 <wneh2023@naver.com>
2026-02-23 10:50:51 +0900

    [#632] fix(common): SwaggerConfig 추가

---
+++ common/src/main/java/com/modeunsa/global/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.modeunsa.global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+  @Value("${custom.swagger.serverUrl}")
+  private String serverUrl;
+
+  @Value("${custom.swagger.description}")
+  private String description;
+
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+        .addServersItem(new Server().url(serverUrl).description(description));
+  }
+}
