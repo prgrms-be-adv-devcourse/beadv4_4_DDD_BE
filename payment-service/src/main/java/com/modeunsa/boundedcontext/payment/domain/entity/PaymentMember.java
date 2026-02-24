2266067ad86e7e9b34e7a5e05434a0f46a72c4f6
kjuiop <copying_y@naver.com>
2026-02-24 15:39:04 +0900

    [#656] fix: payment member id 중복 생성

--- payment-service/src/main/java/com/modeunsa/boundedcontext/payment/domain/entity/PaymentMember.java
+++ payment-service/src/main/java/com/modeunsa/boundedcontext/payment/domain/entity/PaymentMember.java
@@ -1,7 +1,5 @@
 package com.modeunsa.boundedcontext.payment.domain.entity;
 
-import static jakarta.persistence.GenerationType.IDENTITY;
-
 import com.modeunsa.boundedcontext.payment.domain.exception.PaymentDomainException;
 import com.modeunsa.boundedcontext.payment.domain.exception.PaymentErrorCode;
 import com.modeunsa.boundedcontext.payment.domain.types.PaymentMemberStatus;
@@ -12,7 +10,6 @@ import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -29,9 +26,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class PaymentMember extends AuditedEntity {
 
-  @Id
-  @GeneratedValue(strategy = IDENTITY)
-  private Long id;
+  @Id private Long id;
 
   private static final String customerKeyPrefix = "CUSTOMER";
 
